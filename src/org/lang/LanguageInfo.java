ff1b2ab17c67439a17be3aa50f1fbc58bb3420bd
Yogeshwaran19 <syogeshwaran19@gmail.com>
2022-05-27 11:53:10 +0530

    new

--- src/org/lang/LanguageInfo.java
+++ src/org/lang/LanguageInfo.java
@@ -1,17 +1,24 @@
 package org.lang;
 
 public class LanguageInfo {
-public void tamilLanguage() {
+	public void tamilLanguage() {
 
+		System.out.println("tamilans speak tamil language");
+	}
 
-	System.out.println("tamilans speak tamil language");
-}
-public void englishLanguage(){
+	public void englishLanguage() {
 
-	System.out.println("americans speak english language");
-}
-public void hindiLanguage()
-{
-	System.out.println("north peoples speaks hindi");
-}
+		System.out.println("americans speak english language");
+	}
+
+	public void hindiLanguage() {
+		System.out.println("north peoples speaks hindi");
+	System.out.println("Language");
+	}
+	public static void main(String[] args) {
+		LanguageInfo a = new LanguageInfo();
+		a.tamilLanguage();
+		a.englishLanguage();
+		a.hindiLanguage();
+	}
 }
